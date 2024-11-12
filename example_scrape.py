b3ec8c3640c4055f8dee1dd05cdd21105b747711
bakhtsingh <46533553+bakhtsingh@users.noreply.github.com>
2024-11-12 08:45:44 -0600

    Update example_scrape.py

--- example_scrape.py
+++ example_scrape.py
@@ -40,7 +40,7 @@ except FileNotFoundError:
 processed_zips = set([record['ZIP Code'] for record in all_church_data])
 
 # Iterate over each ZIP code in the CSV
-for index, zip_code in enumerate(zip_data['zip'][20000:25000], start=20000):
+for index, zip_code in enumerate(zip_data['zip'][23000:23020], start=23000):
     if zip_code in processed_zips:
         print(f"Skipping ZIP code {zip_code} (already processed).")
         continue
